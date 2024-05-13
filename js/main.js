69389d8577f593e919143d3d4cf9b73158c1d8fd
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-13 16:06:46 +0200

    M1 Completato

--- js/main.js
+++ js/main.js
@@ -5,13 +5,38 @@ createApp({
     data() {
         return {
             
+            images: [
+                "./img/01.webp",
+                "./img/02.webp",
+                "./img/03.webp",
+                "./img/04.webp",
+                "./img/05.webp",
+            ],
+            
+            activeImage: 0,
+            
         }
     },
     methods: {
-       
+        checkImageVisibility(indice) {
+            return (indice == this.activeImage) ? 'active' : '';
+        },
+        
+        next() {
+            this.activeImage++;
+        },
+
+        back() {
+            this.activeImage--;
+        },
+
+        mostraSlide(indice) {
+            this.activeImage = indice;
+        }
+
     },
     mounted() {
-        
+       
     }
 
 }).mount('#app')
